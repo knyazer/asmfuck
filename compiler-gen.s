a5b0964b87903d91aeb3180ae92b51a8af1698d1
Roma Knyaz <git@knyaz.tech>
2022-09-19 13:51:27 +0200

    WIP: basic bf compiler

--- compiler-gen.s
+++ compiler-gen.s
@@ -11,6 +11,10 @@
     _jne: .asciz "\n    jne "
     _je: .asciz  "\n    je  "
     _mul: .asciz "\n    mul "
+    _label: .asciz " label_"
+    _colon: .asciz ": \n"
+    _new_line: .asciz "\n"
+    _loop_start_bp: .asciz "\n    cmpb $0, (%rbx)\n    je"
     
     _print: .asciz "\n    movq $1, %rax\n    movq $1, %rdi\n    movq $1, %rdx\n    movzxb (%rbx), %rsi\n    syscall\n"
 
@@ -20,7 +24,7 @@
     _rbx_wrapped: .asciz "(%rbx)"
 
 
-generate_asm_from_bf:
+compile_to_string:
     pushq %rbp              # Push base pointer to stack
     movq %rsp, %rbp         # Base pointer = stack pointer 
     
@@ -38,7 +42,9 @@ generate_asm_from_bf:
     movq %rsi, %r13
 
     # Main loop
+    subq $8, %r12
 gen_main_loop:
+    addq $8, %r12
     # Get the operation on the current block
     movzxb 4(%r12), %r10
     # Get the number of repetitions of the current block
@@ -129,16 +135,83 @@ gen_read:
     jmp gen_main_loop
 
 gen_loop_start:
+    # label_xxx:
+    #     cmpb $0, (%rbx)
+    #     je label_yyy
+
+    # In the next block from the loop start we have stored the address of the block after the loop end
+    # Lets use it in hex form, as a xxx and yyy. We will use the same format for all labels
+    # So, yyy = 8(%r12), qword; xxx = %r12, qword
+    
+    movq %r13, %rdi
+    movq %r12, %rsi
+    call print_label_with_colon
+    movq %rax, %r13
+    
+    # Next line!
+    movq %r13, %rdi
+    movq $_loop_start_bp, %rsi
+    call add_line
+    movq %rax, %r13
+
+    # Last label!
+    movq %r13, %rdi
+    movq 8(%r12), %rsi
+    call print_label
+    movq %rax, %r13
+
+    # and new line
+    movq %r13, %rdi
+    movq $_new_line, %rsi
+    call add_line
+    movq %rax, %r13
+    
+    # Done
+
     jmp gen_main_loop
 
 gen_loop_end:
+    #     jmp label_xxx
+    # label_yyy:
+    movq %r13, %rdi
+    movq $_jmp, %rsi
+    call add_line
+    movq %rax, %r13
+    # Current state: jmp
+    
+    movq %r13, %rdi
+    movq 8(%r12), %rsi
+    call print_label
+    movq %rax, %r13
+    # Current state: jmp label_xxx
+
+    # and new line
+    movq %r13, %rdi
+    movq $_new_line, %rsi
+    call add_line
+    movq %rax, %r13
+    
+    # and label
+    movq %r13, %rdi
+    movq %r12, %rsi
+    call print_label_with_colon
+    movq %rax, %r13
+
+    # Done
+    
     jmp gen_main_loop
 
 # Complex operations are here
 gen_complex_exit:
+    # Just done with the program
+    # Add null terminator
+    incq %r13
+    movb $0, (%r13)
+
     jmp done
 
 gen_complex_start:
+    
     jmp gen_main_loop
 
 gen_complex_end:
@@ -184,6 +257,8 @@ add_line_loop:
     incq %rdi
     incq %rsi
 
+    jmp add_line_loop
+
 add_line_done:
     movq %rdi, %rax # Return the pointer to the end of the string
 
@@ -209,33 +284,20 @@ construct_line_first_loop:
     incq %rdi
     incq %rsi
 
+    jmp construct_line_first_loop
+
 construct_line_first_loop_done:
     # Now, we have reached interesting stuff. The first thing is to render the number of repetitions, which is in %edx. How do we do that? Simple: print 8 digits in hex.
-
-construct_number_loop:
-    # Lets take first 4 bits of the %edx, and transform it into a hex digit
-    movl %edx, %eax
-    andl $0xF, %eax
-    # Now there is a number in %eax, which is between 0 and 15. We need to transform it into a hex digit
-    cmpb $10, %al # If the number is greater or equal than 10, we need to add code of letter 'A' to it
-    jge _cl_add_A_to_al
-    # Otherwise we need to add code of '0'
-    addb $'0', %al
-    jmp _cl_al_done
-
-_cl_add_A_to_al:
-    addb $'A' - 10, %al
     
-_cl_al_done:
-    # Now we have the hex digit in %al, and we need to add it to the dest
-    movb %al, (%rdi)
-    incq %rdi
-    
-    # Now we need to shift the %edx to the right by 4 bits
-    shrl $4, %edx
-    # And repeat the process until %edx is 0
-    cmpl $0, %edx
-    jne construct_number_loop
+    # Firstly, print hex prefix $0x
+    movb $'$', (%rdi)
+    movb $'0', 1(%rdi)
+    movb $'x', 2(%rdi)
+    addq $3, %rdi
+
+    # rdi is correct, rsi is not
+    movl %edx, %esi # Move the number to the second param
+    call print_hex_to_address
 
     # Now we need to add ' , ' to the dest
     movb $' ', (%rdi)
@@ -255,6 +317,8 @@ construct_line_second_loop:
 
     incq %rdi
     incq %rcx
+
+    jmp construct_line_second_loop
     
 construct_line_second_loop_done:
     # Now we need to add '\n' to the dest, as the final thing
@@ -267,6 +331,80 @@ construct_line_second_loop_done:
     movq %rbp, %rsp
     popq %rbp
     ret
+
+print_hex_to_address:
+    construct_number_loop:
+    # Lets take first 4 bits of the %esi, second param, and transform it into a hex digit
+    movq %rsi, %rax
+    andq $0xF, %rax
+    # Now there is a number in %eax, which is between 0 and 15. We need to transform it into a hex digit
+    cmpb $10, %al # If the number is greater or equal than 10, we need to add code of letter 'A' to it
+    jge _cl_add_A_to_al
+    # Otherwise we need to add code of '0'
+    addb $'0', %al
+    jmp _cl_al_done
+
+_cl_add_A_to_al:
+    addb $'A' - 10, %al
+    
+_cl_al_done:
+    # Now we have the hex digit in %al, and we need to add it to the dest
+    movb %al, (%rdi)
+    incq %rdi
+    
+    # Now we need to shift the %edx to the right by 4 bits
+    shrq $4, %rsi
+    # And repeat the process until %rsi is 0
+    cmpq $0, %rsi
+    jne construct_number_loop
+    
+    # Now we have to return the new pointer
+    movq %rdi, %rax
+    ret
+
+print_label:
+    movq %rdi, %r8
+    movq %rsi, %r9
+    
+    movq %r8, %rdi
+    movq $_label, %rsi
+    call add_line
+    movq %rax, %r8
+    # Current state: label_
+    
+    movq %r8, %rdi
+    movq %r9, %rsi
+    call print_hex_to_address
+    movq %rax, %r8
+    # Current state: label_1234
+    
+    movq %r8, %rax
+    ret
+
+print_label_with_colon:
+    movq %rdi, %r8
+    movq %rsi, %r9
+    
+    movq %r8, %rdi
+    movq $_label, %rsi
+    call add_line
+    movq %rax, %r8
+    # Current state: label_
+    
+    movq %r8, %rdi
+    movq %r9, %rsi
+    call print_hex_to_address
+    movq %rax, %r8
+    # Current state: label_1234
+    
+    movq %r8, %rdi
+    movq $_colon, %rsi
+    call add_line
+    movq %rax, %r8
+    # Current state: label_1234:
+    
+    movq %r8, %rax
+    ret    
 
 done:
     # Restore callee-saved registers
